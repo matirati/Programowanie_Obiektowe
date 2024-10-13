905650942af566088cf93e16548269691d1eed21
matirati <145115687+matirati@users.noreply.github.com>
2024-10-13 09:34:05 +0200

    Add files via upload

---
+++ LAB_1/LAB1/LAB1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Kopiowanie tekstu"); //ctrl + d
+Console.WriteLine("Komentowanie"); //ctrl + k + c ctrl + k + u
+
+
+//int a, b = 34;
+//double c = 34.34;
+//short s = 1;
+//long logbA = 12313244214214;
+//char znak = 'a';
+//string namee = "jan";
+//bool type = true;
+
+//Console.Write("wartosc liczby b = " + b + "\n");
+//Console.WriteLine("s =" + s);
+
+//Console.WriteLine("Podaj imie");
+//string name = Console.ReadLine();
+//Console.WriteLine("Twoje imie: " + name);
+
+
+//Console.WriteLine("Podaj liczbe");
+//int number = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Podana liczba: " + number);
+
+Console.WriteLine("Podaj liczbe");
+string input = Console.ReadLine();
+int liczba = int.Parse(input);
